996cd73d1f8ddf11dc18e915ad32b6f2811fd280
GigaCash <gigacash7@gmail.com>
2018-08-24 23:38:03 +0100

    Replace

--- setupmn.sh
+++ setupmn.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 TMP_FOLDER=$(mktemp -d)
-CONFIG_FILE='gigacash.conf'
+CONFIG_FILE='Gigacash.conf'
 CONFIGFOLDER='/root/.gigacashdir'
-COIN_DAEMON='gigacashd'
-COIN_CLI='gigacash-cli'
+COIN_DAEMON='Gigacashd'
+COIN_CLI='Gigacash-cli'
 COIN_PATH='/usr/local/bin/'
 COIN_REPO='https://github.com/GigaCash/GigaCash'
 COIN_TGZ='https://raw.githubusercontent.com/GigaCash/GigaCash-Masternode-Setup/master/gigacash.tar.gz'
 COIN_ZIP=$(echo $COIN_TGZ | awk -F'/' '{print $NF}')
 COIN_NAME='GigaCash'
-COIN_PORT=5078 #Updated Port
-RPC_PORT=5178
+COIN_PORT=5077 #Updated Port
+RPC_PORT=5177
 
 NODEIP=$(curl -s4 icanhazip.com)
 
